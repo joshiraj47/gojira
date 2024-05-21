dbb9c670a3bdc202a2dbeb470d4be20b37f96618
Raj Joshi <rajjos@cisco.com>
2024-05-21 22:56:23 +0530

    update vercel domains

--- server/api/index.js
+++ server/api/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'http://192.168.0.102:3000'
+    origin: 'https://gojira-frontend.vercel.app'
 }));
 app.use('/images', express.static('../src/avatars'))
 
