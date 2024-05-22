cc0d9f0fb391a3c49abe6ca4c40ece987865835e
Raj Joshi <rajjos@cisco.com>
2024-05-22 11:30:04 +0530

    change cookie

--- server/api/index.js
+++ server/api/index.js
@@ -39,7 +39,9 @@ app.post("/login", async (req, res) => {
                 UserModel.updateOne({email: userDoc.email}, {$set: {lastLogin: new Date().getTime()}}).then((updatedDoc) => {
                     jwt.sign({email: userDoc.email, id: userDoc._id}, jwtSecret, {}, (err, token) => {
                         if (err) throw err;
-                        return res.cookie("token", token).json("success");
+                        return res
+                            .cookie("token", token, { withCredentials: true, sameSite: "none", secure: true, httpOnly: true })
+                            .json("success");
                     });
                 });
 
